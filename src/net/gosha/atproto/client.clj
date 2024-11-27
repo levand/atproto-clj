d68066e90c70e55c2699985031f260a28accc0b6
Gosha Tcherednitchenko <git@gosha.net>
2024-11-27 17:08:46 +0000

    feat(client): minimal auth and basic requests
    This commit adds some scaffolding:
    - Basic configuration in the core namespace
    - Exchanging app password for an auth token in the client ns
    - A request function and a couple of convenience methods in client ns
    
    Still needs tests!

--- src/net/gosha/atproto/client.clj
+++ src/net/gosha/atproto/client.clj
@@ -1,13 +1,65 @@
 (ns net.gosha.atproto.client
   (:require [org.httpkit.client :as http]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.pprint :refer [pprint]]
+            [net.gosha.atproto.core :as core]))
 
-(defn make-request
-  "Make an HTTP request to the ATProto API"
-  [method url body headers]
-  (let [options {:method method
-                 :url url
-                 :headers headers
-                 :body (when body (json/write-str body))}
-        response @(http/request options)]
-    (update response :body json/read-str :key-fn keyword)))
+(defn request
+  "Make an HTTP request to the atproto API.
+  - `method`: HTTP method (:get, :post, etc.)
+  - `endpoint`: API endpoint (relative to `:base-url`)
+  - `body`: Request body (optional)
+  - `headers`: Additional headers (optional)
+  - `retries`: Number of retries for transient failures"
+  [method endpoint & [{:keys [body headers retries] :or {retries 3}}]]
+  (let [{:keys [base-url auth-token]} @core/config]
+    (when-not base-url
+      (throw (ex-info "SDK not initialised: missing base-url" {})))
+    (let [url (str base-url endpoint)
+          options {:method method
+                   :url url
+                   :headers (merge {"Authorization" (str "Bearer " auth-token)
+                                    "Content-Type" "application/json"}
+                                   headers)
+                   :body (when body (json/write-str body))}]
+      (loop [attempt 0]
+        (let [response (try
+                         {:success true
+                          :result @(http/request options)}
+                         (catch Exception e
+                           {:success false
+                            :error e}))]
+          (if (:success response)
+            (let [result (:result response)]
+              (if (<= 200 (:status result) 299)
+                (update result :body json/read-str :key-fn keyword)
+                (throw (ex-info "API request failed"
+                                {:status (:status result)
+                                 :body (:body result)}))))
+            (if (>= attempt retries)
+              (throw (:error response))
+              (do
+                (Thread/sleep (* 100 (inc attempt)))
+                (recur (inc attempt))))))))))
+
+;; Convenience functions
+(defn get-req
+  "Perform a GET request to the atproto API."
+  [endpoint & [options]]
+  (request :get endpoint options))
+
+(defn post-req
+  "Perform a POST request to the atproto API."
+  [endpoint body & [options]]
+  (request :post endpoint (merge options {:body body})))
+
+(defn authenticate!
+  "Authenticate with the atproto API using an app password.
+   Updates configuration with auth token."
+  []
+  (let [endpoint "/xrpc/com.atproto.server.createSession"
+        response (post-req endpoint {:identifier (:username @core/config)
+                                 :password (:app-password @core/config)}
+                       {:base-url (:base-url @core/config)})
+        token (get-in response [:body :accessJwt])]
+    (swap! core/config assoc :auth-token token)))
